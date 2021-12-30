64afb5cc70ed4cd801ba7906a59524eee07b9e61
live77 <535157110@qq.com>
2021-12-30 23:42:10 +0800

    add contribute force graph

--- src/cmd/subcmd/genContributeGraph.go
+++ src/cmd/subcmd/genContributeGraph.go
@@ -1,6 +1,7 @@
 package subcmd
 
 import (
+	"fmt"
 	"github.com/gitdog7/gitdog/src/visualize/contributor"
 	"github.com/gitdog7/gitdog/src/visualize/painter"
 	log "github.com/google/logger"
@@ -15,23 +16,31 @@ var GenContributeGraphCmd = &cobra.Command{
 	Long:  `generate contribute graph`,
 	Run: func(cmd *cobra.Command, args []string) {
 		// load repository
-		repository := loadRepository(cmd)
+		repo := loadRepository(cmd)
+		workdir, _ := cmd.Flags().GetString("workdir")
 
 		// visualize
-		viz := contributor.ContributeGraphViz{
-			Repo: repository,
+		option := contributor.ContributeGraphOption{
 			TopK: 100,
 			Type: "circular",
 		}
 
-		// generate contributor graph
-		graph := viz.GenerateGraph()
-		workdir, _ := cmd.Flags().GetString("workdir")
-		outputPath := workdir + "/" + getGraphSaveName(graph.Title.Title) + ".html"
-		painter.PaintGraph(graph, outputPath)
-		log.Infof("contribute graph: %v generated successfully.", outputPath)
+		// generate contributor graph (circular)
+		graph1 := contributor.GenerateGraph(repo, option)
+		outputPath1 := workdir + "/" + getGraphSaveName(graph1.Title.Title) + ".html"
+		painter.PaintGraph(graph1, outputPath1)
+		log.Infof("contribute graph: %v generated successfully.", outputPath1)
+
+		// generate contribute graph (force)
+		option.Type = "force"
+		graph2 := contributor.GenerateGraph(repo, option)
+		outputPath2 := workdir + "/" + getGraphSaveName(graph2.Title.Title) + ".html"
+		painter.PaintGraph(graph2, outputPath2)
+		log.Infof("contribute graph: %v generated successfully.", outputPath2)
 
 		currentOSWD, _ := os.Getwd()
-		log.Infoln("file://" + currentOSWD + "/" + outputPath)
+		fmt.Println("-----------------------------")
+		fmt.Println("file://" + currentOSWD + "/" + outputPath1)
+		fmt.Println("file://" + currentOSWD + "/" + outputPath2)
 	},
 }
